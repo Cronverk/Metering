4afbf73b010507a22ca2ba25d52e5666a123d040
yaroslavromanyuta <yaroslavromanyuta@mail.ru>
2016-03-12 12:03:36 +0200

    add valid

---
+++ app/src/main/java/com/example/grass/metering/validation/MyValidation.java
@@ -0,0 +1,76 @@
+package com.example.grass.metering.validation;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v1.DbxClientV1;
+import com.dropbox.core.v1.DbxEntry;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Locale;
+
+import static com.example.grass.metering.Constants.DRIVE_URL;
+import static com.example.grass.metering.Constants.TAG;
+
+/**
+ * Created by Yaroslav on 08.03.2016.
+ */
+public class MyValidation {
+    boolean valid = false;
+    SharedPreferences preferences;
+
+    public boolean isValid(String imei, Context context) {
+
+        preferences = context.getSharedPreferences("my_settings" , Context.MODE_PRIVATE);
+        DbxRequestConfig config = new DbxRequestConfig("taxation", Locale.getDefault().toString());
+        DbxClientV1 clientV2 = new DbxClientV1(config,DRIVE_URL);
+        Log.d(TAG, "isValid: ");
+        //String sr =
+        FileOutputStream outStream = null;
+        StringBuilder text = null;
+        try {
+            Log.d(TAG, "isValid: " + clientV2.getAccountInfo().email);
+
+            outStream = new FileOutputStream(context.getFilesDir() + "/licenBasHiMeter.txt");
+            DbxEntry.File downloadFile = clientV2.getFile("/licenBasHiMeter.txt",null, outStream);
+            Log.d(TAG, "isValid: file " + downloadFile.asFile().toString());
+            outStream.close();
+
+            text = new StringBuilder();
+            BufferedReader br = new BufferedReader(new FileReader(context.getFilesDir() + "/licenBasHiMeter.txt"));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            br.close();
+
+            Log.d(TAG, "isValid: chek " + text.toString().contains(imei));
+            if (text.toString().contains(imei)){
+                Log.d(TAG, "isValid: works");
+                Calendar dayD = Calendar.getInstance();
+                dayD.add(Calendar.DATE,365);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putLong("dayD",dayD.getTimeInMillis());
+                editor.apply();
+
+                return true;
+            }
+        }
+        catch (DbxException | IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "isValid: "+ e.toString());
+        }
+        return false;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/grass/metering/validation/MyValidator.java
@@ -0,0 +1,43 @@
+package com.example.grass.metering.validation;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import static com.example.grass.metering.Constants.TAG;
+
+/**
+ * Created by Yaroslav on 08.03.2016.
+ */
+public class MyValidator extends AsyncTask<Void,Void,Boolean> {
+
+    String imei;
+    TelephonyManager manager;
+    ValidationCallback callback;
+    Context context;
+
+    public MyValidator(Context context, ValidationCallback callback) {
+        super();
+        manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        imei = manager.getDeviceId();
+        this.context = context;
+        this.callback = callback;
+        Log.d(TAG, "MyValidator: " + "IMEI" + imei);
+    }
+
+
+
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        Log.d(TAG, "doInBackground() returned: " );
+        MyValidation myValidation = new MyValidation();
+        return myValidation.isValid(imei,context);
+    }
+
+    @Override
+    protected void onPostExecute(Boolean aBoolean) {
+        callback.valid(aBoolean);
+    }
+}

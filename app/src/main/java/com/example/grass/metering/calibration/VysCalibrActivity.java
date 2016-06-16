c0c5d6823d6ac2107c6749e738acebd43196104a
3lolo <wopozniack@gmail.com>
2016-06-16 21:43:47 +0300

    fix

--- app/src/main/java/com/example/grass/metering/calibration/VysCalibrActivity.java
+++ app/src/main/java/com/example/grass/metering/calibration/VysCalibrActivity.java
@@ -222,8 +222,7 @@ public class VysCalibrActivity extends Activity implements View.OnClickListener,
         int degree = (int) value;
         double rawMinute = Math.abs((value % 1) * 60);
         int minute = (int) rawMinute;
-        int second = (int) Math.round((rawMinute % 1) * 60);
-        return String.format("%d° %d′ %d″", degree,minute,second);
+        return String.format("%d° %d′ ", degree,minute);
     }
     public void stopTask() {
         task.stopTask();
